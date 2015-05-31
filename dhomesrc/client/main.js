fba65719a6d4ea505e0324c7dd2d5e4c8eec8fa0
frankc201106 <franklai@Franks-MacBook-Pro.local>
2015-05-31 11:30:49 -0400

    Project dialog/form implmented

--- dhomesrc/client/main.js
+++ dhomesrc/client/main.js
@@ -5,6 +5,15 @@ if (Meteor.isClient){
 Template.projects.helpers({
   projectList: function () {
     return Projects.find();
+  },
+  showProjectDialog: function() {
+  	return Session.get('showProjectDialog');
   }
 });
 }
+/*Template.projects.event({
+	'click .save'
+});
+
+}
+*/
